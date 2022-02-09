3cddd491a85caf6a1132d28abb4a866d4bafc568
GoosebumpsPala <82609231+GoosebumpsPala@users.noreply.github.com>
2022-02-09 17:46:58 +0100

    Update antiInsult.js

--- files/antiInsult.js
+++ files/antiInsult.js
@@ -1,7 +1,29 @@
-module.exports = (message, client, config, Discord, prefix) => {
+module.exports = async (message, client, config, Discord, prefix) => {
 
-    if (msgSplit[0].toLowerCase() != prefix + "kick") return;
-
+    if (message.author.bot) return;
     
+    const isInvite = (guild, code) => new Promise((resolve) => {
+        guild.fetchInvites().then((invites) => {
+            resolve(invites.some((value) => value[0] === code));
+        });
+    });
+    
+    const code = message.content.split("discord.gg/")[1];
+    if (message.content.includes("discord.gg/")) {
+            const isOurInvite = await isInvite(message.guild, code);
+            if(!IsOurInvite) {
+                message.delete();
+                message.channel.send(":warning: **La pub n'est pas autorisée !**");
+            }
+        });
+    
+    const code = message.content.split("discord.com/invite/")[1];
+    if (message.content.includes("discord.com/invite/")) {
+            const isOurInvite = await isInvite(message.guild, code);
+            if(!IsOurInvite) {
+                message.delete();
+                message.channel.send(":warning: **La pub n'est pas autorisée !**");
+            }
+        });
 
 }
