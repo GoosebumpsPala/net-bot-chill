63e7ebf118d4295af3add9d0520965631cc2ccb7
Bears <goosebumps.paladium@gmail.com>
2022-02-13 13:39:58 +0100

    Test channel manager

--- index.js
+++ index.js
@@ -172,3 +172,25 @@ client.on("message", message => {
         });
     })
 })*/
+
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    const user = client.users.fetch(newState.id)
+    const member = newState.guild.member(user)
+
+    if (!oldState && newState.channel.id === "942397107376111636") {
+        const channel = newState.guild.channels.create("Salon de " + user.tag, {
+            type: 'voice',
+            parent: newState.channel.parent,
+        });
+        member.voice.setChannel(channel);
+        voiceCollection.set(user.id, channel.id);
+
+    } else if (!newState.channel) {
+        if (oldState.channelID === voiceCollection.get(newState.id)) {
+            oldState.channel.delete();
+            return;
+        }
+    }
+    
+  })
